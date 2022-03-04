316e50b61c179f7d75cf5819b4a56e9177d19643
Alireza Rashvand <a.r.rashvand@outlook.com>
2022-03-04 15:41:11 +0330

    NewCreateProject

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,196 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <b-table
+      striped
+      hover
+      sticky-header
+      :items="Values"
+      :fields="fields"
+      label-sort-asc=""
+      label-sort-desc=""
+      label-sort-clear=""
+      :sort-by.sync="sortBy"
+      :sort-desc.sync="sortDesc"
+    >
+      <template #cell(username)="data">
+        {{ data.item.username }}
+      </template>
+
+      <template #cell(classical_rating)="data">
+        {{ data.item.classical_rating }}
+
+        <b-icon
+          v-if="data.item.classical_prog > 0"
+          icon="arrow-up"
+          variant="success"
+          rotate="45"
+        ></b-icon>
+        <b-icon
+          v-if="data.item.classical_prog < 0"
+          icon="arrow-down"
+          variant="danger"
+          rotate="-45"
+        ></b-icon>
+      </template>
+
+      <template #cell(rapid_rating)="data">
+        {{ data.item.rapid_rating }}
+
+        <b-icon
+          v-if="data.item.rapid_prog > 0"
+          icon="arrow-up"
+          variant="success"
+          rotate="45"
+        ></b-icon>
+        <b-icon
+          v-if="data.item.rapid_prog < 0"
+          icon="arrow-down"
+          variant="danger"
+          rotate="-45"
+        ></b-icon>
+
+      </template>
+      <template #cell(blitz_rating)="data">
+        {{ data.item.blitz_rating }}
+
+        <b-icon
+          v-if="data.item.blitz_prog > 0"
+          icon="arrow-up"
+          variant="success"
+          rotate="45"
+        ></b-icon>
+        <b-icon
+          v-if="data.item.blitz_prog < 0"
+          icon="arrow-down"
+          variant="danger"
+          rotate="-45"
+        ></b-icon>
+
+      </template>
+      <template #cell(bullet_rating)="data">
+        {{ data.item.bullet_rating }}
+
+        <b-icon
+          v-if="data.item.bullet_prog > 0"
+          icon="arrow-up"
+          variant="success"
+          rotate="45"
+        ></b-icon>
+        <b-icon
+          v-if="data.item.bullet_prog < 0"
+          icon="arrow-down"
+          variant="danger"
+          rotate="-45"
+        ></b-icon>
+      </template>
+    </b-table>
   </div>
 </template>
 
+  
+
 <script>
+import axios from "axios";
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  data() {
+    return {
+      users: ["rashvand", "HamidrezaBagheri", "mrseif"],
+      Values: [],
+      sortBy: "classical_rating",
+      sortDesc: true,
+      fields: [
+        {
+          key: "username",
+          label: "username",
+        },
+        {
+          key: "classical_rating",
+          label: "classical",
+          sortable: true,
+        },
+        {
+          key: "rapid_rating",
+          label: "rapid",
+          sortable: true,
+        },
+        {
+          key: "blitz_rating",
+          label: "blitz",
+          sortable: true,
+        },
+        {
+          key: "bullet_rating",
+          label: "bullet",
+          sortable: false,
+        },
+      ],
+    };
+  },
+
+  created() {
+    this.apiCall();
+  },
+  methods: {
+    apiCall() {
+      this.users.forEach((item) => {
+        const promise = axios({
+          method: "get",
+          url: `https://lichess.org/api/user/${item}`,
+        });
+        promise.then((response) => {
+          this.Values.push({
+            url: response.data.url,
+            id: response.data.id,
+            username: response.data.username,
+            online: response.data.online,
+            firstName: response.data.profile.firstName,
+            lastName: response.data.profile.lastName,
+            country: response.data.profile.country,
+            blitz_games: response.data.perfs.blitz.games,
+            blitz_rating: response.data.perfs.blitz.rating,
+            blitz_rd: response.data.perfs.blitz.rd,
+            blitz_prog: response.data.perfs.blitz.prog,
+
+            bullet_games: response.data.perfs.bullet.games,
+            bullet_rating: response.data.perfs.bullet.rating,
+            bullet_rd: response.data.perfs.bullet.rd,
+            bullet_prog: response.data.perfs.bullet.prog,
+
+            classical_games: response.data.perfs.classical.games,
+            classical_rating: response.data.perfs.classical.rating,
+            classical_rd: response.data.perfs.classical.rd,
+            classical_prog: response.data.perfs.classical.prog,
+
+            rapid_games: response.data.perfs.rapid.games,
+            rapid_rating: response.data.perfs.rapid.rating,
+            rapid_rd: response.data.perfs.rapid.rd,
+            rapid_prog: response.data.perfs.rapid.prog,
+          });
+        });
+      });
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+td,
+th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+tr:nth-child(even) {
+  background-color: #dddddd;
 }
 </style>
