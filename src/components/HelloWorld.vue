a8e66fb02c7e6cce61d29d94c4f9d57d8d28df6d
Alireza Rashvand <a.r.rashvand@outlook.com>
2022-03-10 01:24:15 +0330

    Flag

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,23 @@
 <template>
   <div>
+    <div>
+      <P>
+        <h2>
+          <b-link style=" text-decoration: none"
+            target="_blank"
+            class="font-weight-bold"
+            href="https://lichess.org/team/vjya9K6q"
+          >
+            <span> باشگاه ستارگان </span>
+          </b-link>
+        </h2>
+      </P>
+    </div>
     <b-table
       striped
+      responsive 
       hover
       :busy.sync="isBusy"
-      sticky-header
       :items="Values"
       :fields="fields"
       label-sort-asc=""
@@ -19,14 +32,35 @@
           <strong>Loading...</strong>
         </div>
       </template>
+
       <template #cell(index)="data">
         {{ data.index + 1 }}
       </template>
 
+      <template #cell(flag)="data">
+        <country-flag
+          v-if="data.item.url !== ''"
+          :country="data.item.country"
+          size="small"
+        />
+      </template>
+
       <template #cell(username)="data">
-        <b-link target="_blank" :href="data.item.url">{{
-          data.item.username
-        }}</b-link>
+        <b-icon
+          icon="circle"
+          v-if="!data.item.online"
+        ></b-icon>
+
+       <b-icon  v-if="data.item.online"
+          icon="circle-fill"
+          variant="success"
+        ></b-icon>
+
+        <b-link target="_blank" :href="data.item.url">
+       <span class="">
+         {{ data.item.username }}
+         </span>   
+        </b-link>
       </template>
 
       <template #cell(classical_rating)="data">
@@ -162,7 +196,8 @@ export default {
         "Tavakolian3003",
         "setareganchess",
         "MREZA20",
-        "pishevar","RebelJohnny"
+        "pishevar",
+        "RebelJohnny",
       ],
       Values: [],
       sortBy: "classical_rating",
@@ -170,30 +205,35 @@ export default {
       fields: [
         {
           key: "index",
-          label: "Rating",
+          label: "#",
+        },
+        {
+          key: "flag",
+          label: "",
         },
         {
           key: "username",
-          label: "username",
+          label: "Player",
+          sortable: false,
         },
         {
           key: "classical_rating",
-          label: "classical",
+          label: "Classical",
           sortable: true,
         },
         {
           key: "rapid_rating",
-          label: "rapid",
+          label: "Rapid",
           sortable: true,
         },
         {
           key: "blitz_rating",
-          label: "blitz",
+          label: "Blitz",
           sortable: true,
         },
         {
           key: "bullet_rating",
-          label: "bullet",
+          label: "Bullet",
           sortable: false,
         },
       ],
--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
+import CountryFlag from 'vue-country-flag'
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+Vue.use(CountryFlag)
 
 Vue.config.productionTip = false
 
